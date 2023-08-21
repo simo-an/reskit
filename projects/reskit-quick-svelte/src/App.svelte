db6110594547c65463c6ba937cefcc5908d5f71c
seimor@126.com <GSummer1984>
2023-08-21 08:10:04 +0800

    test: add quick demo of extract number

--- projects/reskit-quick-svelte/src/App.svelte
+++ projects/reskit-quick-svelte/src/App.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   // import ExtractRoom from "./ExtractRoom.svelte";
-  import ExtractLocation from "./ExtractLocation.svelte";
+  // import ExtractLocation from "./ExtractLocation.svelte";
+  import ExtractNumber from "./ExtractNumber.svelte";
 </script>
 
 <main>
-  <ExtractLocation />
+  <ExtractNumber />
 </main>
 
 <style>
